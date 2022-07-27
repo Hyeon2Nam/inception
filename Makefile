2bf00e30cf09f5ca9dcdd22872128c6efb3f2d17
Hyeon yi Nam <hyenam@c3r7s7.42seoul.kr>
2022-07-27 13:28:05 +0900

    Fix Makefile

--- Makefile
+++ Makefile
@@ -3,13 +3,13 @@
 all :
 	bash ./srcs/requirements/tools/first_set.sh
 	bash ./srcs/requirements/tools/install_docker.sh
-	docker-compose -f /srcs/docker-compose.yml up -d --build
+	docker-compose -f ./srcs/docker-compose.yml up -d --build
 
 up:
-	docker-compose -f /srcs/docker-compose.yml up -d
+	docker-compose -f ./srcs/docker-compose.yml up -d
 
 down:
-	docker-compose -f /srcs/docker-compose.yml down
+	docker-compose -f ./srcs/docker-compose.yml down
 
 clean: down
 	docker system prune
--- srcs/requirements/tools/first_set.sh
+++ srcs/requirements/tools/first_set.sh
@@ -2,18 +2,18 @@
 
 apt-get update
 
-sudo apt-get install -y apt-transport-https \
-ca-certificates \
-curl \
-software-properties-common \
-vim \
-systemd
+sudo apt-get install -y apt-transport-https
+sudo apt-get install -y ca-certificates
+sudo apt-get install -y curl
+sudo apt-get install -y software-properties-common
+sudo apt-get install -y vim
+sudo apt-get install -y systemd
 
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 8B48AD6246925553
 
 sudo apt-get update
 
-rm -rf resolv.conf
+rm -rf /etc/resolv.conf
 cp ./resolv.conf /etc/
 
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
