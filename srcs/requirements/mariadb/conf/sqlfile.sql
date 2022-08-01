f2313e7427a291ef781ab8ebf21936c22e581a59
Hyeon yi Nam <hyenam@c5r3s7.42seoul.kr>
2022-08-01 17:55:39 +0900

    Fix wordpress docker file

--- srcs/requirements/mariadb/conf/sqlfile.sql
+++ srcs/requirements/mariadb/conf/sqlfile.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE wordpress;
 CREATE USER 'hyenam'@'%' IDENTIFIED BY 'asdf123';
 GRANT ALL PRIVILEGES ON wordpress.* TO 'hyenam'@'%';
-FLUSH PRIVILEGES;
 ALTER USER 'root'@'localhost' IDENTIFIED BY 'asdf123';
+FLUSH PRIVILEGES;
